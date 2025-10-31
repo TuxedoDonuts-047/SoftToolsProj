cfde37c6dd31b066af30ca0caac4aeb251cfff70
Student Divine Okhaus (TL_KCPSD_B_Y3) <T00233163@mymtu.ie>
2025-10-31 15:09:27 +0000

    Login & Registration UI completed, while dashboard is in progress

---
+++ LogibForm/StaffDashBoard.cs
@@ -0,0 +1,42 @@
+﻿using FrameworkTest;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LogibForm
+{
+    public partial class StaffDashBoard : Form
+    {
+        public StaffDashBoard()
+        {
+            InitializeComponent();
+            // Makes the SATA UI Button transparent to match the gradient panel
+            btnOverView.CheckedBackground = Color.Transparent;
+            btnOverView.NormalBackground = Color.Transparent;
+            
+            btnWishlist.CheckedBackground = Color.Transparent;
+            btnWishlist.NormalBackground = Color.Transparent;
+
+            btnLibrary.CheckedBackground = Color.Transparent;
+            btnLibrary.NormalBackground = Color.Transparent;
+
+            btnCart.CheckedBackground = Color.Transparent;
+            btnCart.NormalBackground = Color.Transparent;
+
+            btnLogout.CheckedBackground = Color.Transparent;
+            btnLogout.NormalBackground = Color.Transparent;
+
+        }
+
+        private void StaffDashBoard_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
